4691c6feeef7ba709127a2511ac9513fd17a1d93
catalin.rosu <d9f10ee5c4267b63b9801077a96e4f379057133a>
2021-02-21 13:26:39 +0200

    add all

--- src/api.js
+++ src/api.js
@@ -1,82 +1,9 @@
 const express = require("express");
 const serverless = require("serverless-http");
 
-import data from "./data.js";
-const data = {
-  products: [
-    {
-      title: "Tavita cafea",
-      description: "Tavita cafea personalzata bla bla bla bbla bla bla bla bla",
-      id: "22",
-      img: "https://firebasestorage.googleapis.com/v0/b/redwoodstore-1613483240446.appspot.com/o/tavaCafea.jpeg?alt=media&token=ca99a697-aa77-43bf-8600-2391a1a332ff"
-    },
-    {
-      title: "Litere de lemn ",
-      description: "Litere de lemn de fag bbla bla bla bla bla bbla bla bla bla bla ",
-      id: "22",
-      img: "https://firebasestorage.googleapis.com/v0/b/redwoodstore-1613483240446.appspot.com/o/WhatsApp%20Image%202021-02-06%20at%2019.58.08.jpeg?alt=media&token=1d74f2a9-742d-4771-b8b5-1634c2dc35ef"
-    },
-    {
-      title: "Decoratiuni perete",
-      description: "Tavita cafea personalzata bla bla bla bbla bla bla bla bla",
-      id: "22",
-      img: "https://firebasestorage.googleapis.com/v0/b/redwoodstore-1613483240446.appspot.com/o/WhatsApp%20Image%202021-02-12%20at%2008.20.37.jpeg?alt=media&token=34f6db3d-5ae8-408c-abb4-e76304bbb30d"
-    },
-    {
-      title: "Suport telefon lemn masiv",
-      description: "Tavita cafea personalzata bla bla bla bbla bla bla bla bla",
-      id: "22",
-      img: "https://firebasestorage.googleapis.com/v0/b/redwoodstore-1613483240446.appspot.com/o/WhatsApp%20Image%202021-02-02%20at%2013.06.20.jpg?alt=media&token=2bbc3573-921f-4c01-b848-3cdbffa01e6d"
-    },
-    {
-      title: "Suport telefon lemn masiv",
-      description: "Tavita cafea personalzata bla bla bla bbla bla bla bla bla",
-      id: "22",
-      img: "https://firebasestorage.googleapis.com/v0/b/redwoodstore-1613483240446.appspot.com/o/WhatsApp%20Image%202021-02-02%20at%2013.06.20.jpg?alt=media&token=2bbc3573-921f-4c01-b848-3cdbffa01e6d"
-    },
-    {
-      title: "Suport telefon lemn masiv",
-      description: "Tavita cafea personalzata bla bla bla bbla bla bla bla bla",
-      id: "22",
-      img: "https://firebasestorage.googleapis.com/v0/b/redwoodstore-1613483240446.appspot.com/o/WhatsApp%20Image%202021-02-02%20at%2013.06.20.jpg?alt=media&token=2bbc3573-921f-4c01-b848-3cdbffa01e6d"
-    },
-    {
-      title: "Suport telefon lemn masiv",
-      description: "Tavita cafea personalzata bla bla bla bbla bla bla bla bla",
-      id: "22",
-      img: "https://firebasestorage.googleapis.com/v0/b/redwoodstore-1613483240446.appspot.com/o/WhatsApp%20Image%202021-02-02%20at%2013.06.20.jpg?alt=media&token=2bbc3573-921f-4c01-b848-3cdbffa01e6d"
-    },
-    {
-      title: "Suport telefon lemn masiv",
-      description: "Tavita cafea personalzata bla bla bla bbla bla bla bla bla",
-      id: "22",
-      img: "https://firebasestorage.googleapis.com/v0/b/redwoodstore-1613483240446.appspot.com/o/WhatsApp%20Image%202021-02-02%20at%2013.06.20.jpg?alt=media&token=2bbc3573-921f-4c01-b848-3cdbffa01e6d"
-    },
-    {
-      title: "Suport telefon lemn masiv",
-      description: "Tavita cafea personalzata bla bla bla bbla bla bla bla bla",
-      id: "22",
-      img: "https://firebasestorage.googleapis.com/v0/b/redwoodstore-1613483240446.appspot.com/o/WhatsApp%20Image%202021-02-02%20at%2013.06.20.jpg?alt=media&token=2bbc3573-921f-4c01-b848-3cdbffa01e6d"
-    },
-    {
-      title: "Suport telefon lemn masiv",
-      description: "Tavita cafea personalzata bla bla bla bbla bla bla bla bla",
-      id: "22",
-      img: "https://firebasestorage.googleapis.com/v0/b/redwoodstore-1613483240446.appspot.com/o/WhatsApp%20Image%202021-02-02%20at%2013.06.20.jpg?alt=media&token=2bbc3573-921f-4c01-b848-3cdbffa01e6d"
-    },
-    {
-      title: "Suport telefon lemn masiv",
-      description: "Tavita cafea personalzata bla bla bla bbla bla bla bla bla",
-      id: "22",
-      img: "https://firebasestorage.googleapis.com/v0/b/redwoodstore-1613483240446.appspot.com/o/WhatsApp%20Image%202021-02-02%20at%2013.06.20.jpg?alt=media&token=2bbc3573-921f-4c01-b848-3cdbffa01e6d"
-    }
-  ]
-};
-
+\
 const app = express();
 const router = express.Router();
-
-
 
 router.get("/", (req, res) => {
   res.json({
@@ -85,7 +12,12 @@ router.get("/", (req, res) => {
 });
 
 router.get("/api/products", (req, res) => {
-  res.json(data.products);
+  res.json([{
+    hello: "hi!"
+  },{
+    hello: "hi!"
+  }
+  ]);
 });
 
 app.use(`/.netlify/functions/api`, router);
